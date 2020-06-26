a4cfbe51de7a216dbabd920302ce35f8b3229f12
Ikeh Leonard <leoikeh99@gmail.com>
2020-06-26 14:21:05 +0100

    Finished functionality for cart endpoint

--- routes/cart.js
+++ routes/cart.js
@@ -8,9 +8,14 @@ const config = require("config");
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
 const authUser = require("../middleWare/authUser");
-const { findByIdAndUpdate } = require("../models/Cart");
+const {
+  findByIdAndUpdate,
+  find,
+  findById,
+  findByIdAndRemove,
+} = require("../models/Cart");
 
-router.post("/:id", authUser, async (req, res) => {
+router.post("/", authUser, async (req, res) => {
   try {
     const product = await Product.findOne({ _id: req.params.id });
     if (!product) {
@@ -40,6 +45,65 @@ router.post("/:id", authUser, async (req, res) => {
       );
       res.json(updateAmount);
     }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/", authUser, async (req, res) => {
+  try {
+    const cart = await Cart.find({ user: req.user.id });
+    if (!cart) {
+      return res.status(400).json({ msg: "Nothing in cart" });
+    }
+    res.json(cart);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Update cart
+//private
+router.put(
+  "/:id",
+  [authUser, check("amount", "Input new amount").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { amount } = req.body;
+    try {
+      const cart = await Cart.findById(req.params.id);
+      if (!cart) {
+        return res.status(400).json({ msg: "Item not contained in cart" });
+      }
+      const cartUpdate = await Cart.findByIdAndUpdate(
+        req.params.id,
+        { $set: { amount } },
+        { new: true }
+      );
+      res.json(cartUpdate);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//DELETE AN ITEM FROM CART
+//Private
+router.delete("/:id", authUser, async (req, res) => {
+  try {
+    const cart = await Cart.findById(req.params.id);
+    if (!cart) {
+      return res.status(400).json({ msg: "Item not contained in cart" });
+    }
+
+    await Cart.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Item removed from cart" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
